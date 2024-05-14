8082ff21cb5a698bf8eba1334e493e0407915a29
eunslog <yeseo235@gmail.com>
2024-05-14 20:14:18 +0900

    first

---
+++ src/main/java/com/clubcommunity/ClubcommunityApplication.java
@@ -0,0 +1,13 @@
+package com.clubcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubcommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubcommunityApplication.class, args);
+	}
+
+}
