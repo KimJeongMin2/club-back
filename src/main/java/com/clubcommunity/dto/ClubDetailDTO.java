c09987ba83fc14aa4305a9f0d87658716d13dd7b
eunslog <yeseo235@gmail.com>
2024-06-05 21:23:36 +0900

    동아리 기본정보 등록및수정

---
+++ src/main/java/com/clubcommunity/dto/ClubDetailDTO.java
@@ -0,0 +1,18 @@
+package com.clubcommunity.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+public class ClubDetailDTO {
+    private Long clubId;
+    private String clubName;
+    private String introduction;
+    private String history;
+    private String meetingTime;
+    private byte[] photo;
+    private byte[] staffList;
+}
